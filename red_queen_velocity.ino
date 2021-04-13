833f5e605871efefa04a177555cf90063b5d733b
Kevin Harrington <mad.hephaestus@gmail.com>
2021-04-13 15:39:39 -0400

    fabs the effort

--- red_queen_velocity.ino
+++ red_queen_velocity.ino
@@ -17,7 +17,7 @@ void loop(){
 
 	float deltaPosition = velocity_setpoint*0.001;// change in position expected in 1ms
 	// Only run away the setpoint if effort is not pegged and the velocity is non zero
-	if(motor.getEffort()<0.95 && fabs(velocity_setpoint)>1)
+	if(fabs(motor.getEffort())<0.95 && fabs(velocity_setpoint)>1)
 		data.position_setpoint+=deltaPosition;
 
 	float error = data.position_setpoint-currentPositon;
